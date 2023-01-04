b14d82c8e406dd5f2e1fe02b84ac744c0526a31a
aneese421@gmail.com <aneese421@gmail.com>
2023-01-04 20:30:55 +0500

    Admin signup, login, forget password functionality

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import multer from "multer";
 
 import { verifyToken } from "./middleware/auth.js";
-import { register, login } from "./controllers/employees.js";
+import { register, login, forgetPass } from "./controllers/employees.js";
 import { createUser } from "./controllers/users.js";
 import { createMember } from "./controllers/members.js";
 
@@ -12,22 +13,32 @@ import { createMember } from "./controllers/members.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('assets'));
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+
 
 // Database connection
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://127.0.0.1:27017/courier-router");
 
-// routes
-app.get("/", (req, res) => {
-    res.send("Server, Hello!")
-})
+const upload = multer({ dest: "public/uploads/" });
 
-app.post("/register", register);
+// routes
+app.post("/register", upload.single("avatar"), register);
 app.post("/login", login);
+app.post("/forgetPass", forgetPass);
 app.post("/member", createMember);
 app.post("/userPackage", createUser);
 
+app.get("/", (req, res) => {
+    res.render("admin/admin-home")
+})
+
 app.listen(3000, () => console.log(`Server running on port 3000`));
+
+
+
+
+// TODO: Admin home user input patterns for registeration and login form
